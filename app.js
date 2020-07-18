7030a8ff889e257bf7c0f78b21967db22903a4c5
atmikha <67125630+atmikha@users.noreply.github.com>
2020-07-18 10:38:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function copy() {
+    alert('LOL! Avoid laziness. No copying!');
+
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = 100 % ;
+    alert("wohoo it is summer");
+
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = 100 % ;
+    alert("wohoo it is winter");
+
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = 100 % ;
+    alert("wohoo it is autumn");
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = 100 % ;
+    alert("wohoo it is spring");
+}
+}
